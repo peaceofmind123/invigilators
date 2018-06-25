981555ee12a3dcdf0d9e81fec3640b2d27b97977
ashishthesatan <ashishthesatan@gmail.com>
2018-06-25 20:05:27 +0545

    initial configuration for database connection and TKinter gui

--- code/main.py
+++ code/main.py
@@ -1,6 +1,15 @@
 import mysql.connector as connector
+import tkinter as tk
+from tkinter import ttk
 
+mainWindow = tk.Tk()
+# gui code goes here
+mainWindow.mainloop()
+
+# database connection
 connection = connector.connect(user='root',host='127.0.0.1',database='invigilators')
 
 cursor = connection.cursor()
+
+# execute sql queries on the cursor using cursor.execute(SQL string)
 
