48a6745d6bebcb51c0702c2a814f09073ffd3570
ashishthesatan <ashishthesatan@gmail.com>
2018-06-26 11:34:58 +0545

    Added UI structure and basic navigation

--- code/main.py
+++ code/main.py
@@ -2,14 +2,60 @@ import mysql.connector as connector
 import tkinter as tk
 from tkinter import ttk
 
-mainWindow = tk.Tk()
-# gui code goes here
-mainWindow.mainloop()
+class HomeUI(tk.Tk):
 
-# database connection
-connection = connector.connect(user='root',host='127.0.0.1',database='invigilators')
+    def update_db(self,event):
+        # redirect to update ui which is a child window of self
+        self.update_UI = UpdateUI()
+        self.update_UI.wm_title("Update entries")
+        self.update_UI.mainloop()
 
-cursor = connection.cursor()
 
-# execute sql queries on the cursor using cursor.execute(SQL string)
+    def view_db(self,event):
+        # redirect to view ui
+        self.view_UI = ViewUI()
+        self.view_UI.wm_title("View entries")
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self,*args,**kwargs)
+
+
+        # child UI definitions
+        self.update_UI = None
+        self.view_UI = None
+
+       # button definitions
+        self.btn_updateDB = ttk.Button(self,text="Update/add entries")
+        self.btn_viewDB = ttk.Button(self,text="View entries")
+
+        # callback binds
+        self.btn_updateDB.bind("<Button-1>", self.update_db)
+        self.btn_viewDB.bind("<Button-1>", self.view_db)
+
+        # layout definition
+        self.btn_updateDB.grid(row=0,column=0,columnspan=2)
+        self.btn_viewDB.grid(row=0, column=2,columnspan=2)
+
+
+class UpdateUI(tk.Toplevel): # toplevel since these will inevitably be children windows
+    def __init__(self, *args, **kwargs):
+        tk.Toplevel.__init__(self,*args,**kwargs)
+
+
+class ViewUI(tk.Toplevel):
+    def __init__(self, *args, **kwargs):
+        tk.Toplevel.__init__(self,*args,**kwargs)
+
+
+if __name__ == "__main__":
+    home_ui = HomeUI()
+    home_ui.wm_title("Invigilators Database")
+    home_ui.mainloop()
+
+    # database connection
+    connection = connector.connect(user='root',host='127.0.0.1',database='invigilators')
+
+    cursor = connection.cursor()
+
+    # execute sql queries on the cursor using cursor.execute(SQL string)
 
