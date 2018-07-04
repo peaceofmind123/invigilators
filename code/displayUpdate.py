ceaa79666dc3f20c4db6309b39c96327ffe814fc
ashishthesatan <ashishthesatan@gmail.com>
2018-07-04 20:36:22 +0545

    completed query logic, todo: fix the logical bug in fetching columns

--- code/displayUpdate.py
+++ code/displayUpdate.py
@@ -4,6 +4,7 @@ import uiabstract
 import math
 import mysql.connector.errors as mysqlerrors
 import mysql
+import copy
 # the abstract update form class
 class DisplayUpdateUI(uiabstract.ChildUI):
     def __init__(self, *args, **kwargs):
@@ -14,6 +15,9 @@ class DisplayUpdateUI(uiabstract.ChildUI):
 
         # rows
         self.rows = []
+
+        # data types info
+        self.dataTypes = {}
         # name to identify the table.. since the class is polymorphic
         self.tableName = None
 
@@ -40,6 +44,14 @@ class DisplayUpdateUI(uiabstract.ChildUI):
                     self.fields[column_names[index]].insert(index, row[index])
                     index += 1
 
+            # getting data types
+            self.cursor.execute("select DATA_TYPE from INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='"+table+"';")
+            i = 0
+            datatypes = self.cursor.fetchall()
+            for key,value in self.fields.items():
+                self.dataTypes[key] = datatypes[i][0]
+                i+=1
+
     def displayData(self):
         if self.fields is not None:
 
@@ -56,13 +68,14 @@ class DisplayUpdateUI(uiabstract.ChildUI):
             # row and column iterators for column labels
             r = 1
             c = 0
-
+            finalRowNum=0
             # row iterator inside the loop
 
             # the label of the columns
             for key, value in self.fields.items():
                 self.columnLabels.append(tk.Label(self.container, text=key))
                 self.columnLabels[c].grid(row=r, column=c)
+
                 self.dataEntryMat.append([])  # making the dataEntryMat 2 dimensional
                 self.dataTextVarMat.append([])
 
@@ -74,19 +87,19 @@ class DisplayUpdateUI(uiabstract.ChildUI):
                     self.dataTextVarMat[c][j].set(value[j])
                     # populating the dataEntryMat
 
-                    print(self.dataTextVarMat[c][j].get())
+
                     entry = tk.Entry(self.container, textvariable=self.dataTextVarMat[c][j],state="readonly")
                     self.dataEntryMat[c].append(entry)
                     self.dataEntryMat[c][j].grid(row=j + 2, column=c)
-                    r = j + 2 # saving r for reference
+                    finalRowNum=j+2 # saving row number for reference in edit and save buttons
 
                 # increasing the column number for the next iteration
                 c += 1
             self.btnEdit = ttk.Button(self.container,text="Edit entries",command=self.btnEditHandler)
             self.btnSave = ttk.Button(self.container,text="Save entries",command=self.btnSaveHandler)
             num_of_cols = len(self.fields)
-            self.btnEdit.grid(row=r+1,column = 0,columnspan = math.ceil(num_of_cols/2))
-            self.btnSave.grid(row=r+1,column = 1,columnspan = num_of_cols-math.floor(num_of_cols/2))
+            self.btnEdit.grid(row=finalRowNum+1,column = 0,columnspan = math.ceil(num_of_cols/2))
+            self.btnSave.grid(row=finalRowNum+1,column = 1,columnspan = num_of_cols-math.floor(num_of_cols/2))
 
     def getRows(self):
         # input: {key1: [a1,a2,...,aj],key2:[b1,b2,...,bj],...,keyi:[x1,x2...xi],...}
@@ -110,8 +123,23 @@ class DisplayUpdateUI(uiabstract.ChildUI):
 
     def btnSaveHandler(self):
 
+
+        # getting the names of the primary key fields
+        self.cursor.execute("SHOW KEYS FROM "+self.tableName+" WHERE Key_name = 'PRIMARY'")
+        primary_cols_info = self.cursor.fetchall()
+        primary_cols_names=[] # this array contains the names of the columns of the superkey
+
+        # saving the primary key values for where clause reference
+        primary_key_vals = {}
+        i=0
+        for entry in primary_cols_info:
+            primary_cols_names.append(entry[4]) # the column name is the fourth entry in the info data
+            primary_key_vals[primary_cols_names[i]] = copy.deepcopy(self.fields[primary_cols_names[i]]) # deepcopy reqd
+            i+=1
+
         # updating the self.fields data structure according to user input
-        c=0
+        c = 0
+
         for key, value in self.fields.items():
             dataCurr = self.dataEntryMat[c]
             for r in range(len(dataCurr)):
@@ -121,12 +149,6 @@ class DisplayUpdateUI(uiabstract.ChildUI):
 
         # flattening the dictionary into rows
         self.getRows()
-        # getting the names of the primary key fields
-        self.cursor.execute("SHOW KEYS FROM "+self.tableName+" WHERE Key_name = 'PRIMARY'")
-        primary_cols_info = self.cursor.fetchall()
-        primary_cols_names=[] # this array contains the names of the columns of the superkey
-        for entry in primary_cols_info:
-            primary_cols_names.append(entry[4]) # the column name is the fourth entry in the info data
 
         # the final update query
         i=0
@@ -135,8 +157,12 @@ class DisplayUpdateUI(uiabstract.ChildUI):
             j=0
             for key,value in self.fields.items():
                 queryString+=key
-                queryString+="="+row[j]
-
+                queryString+="="
+                if self.dataTypes[key]=="varchar":
+                    queryString+="'"
+                queryString+=row[j]
+                if self.dataTypes[key]=="varchar":
+                    queryString+="'"
                 # except for the last key=value declaration, append a comma at the end
                 if j!=len(row)-1:
                     queryString+=", "
@@ -146,9 +172,15 @@ class DisplayUpdateUI(uiabstract.ChildUI):
             l=0
             for name in primary_cols_names:
                 queryString += name+"="
-                queryString += self.fields[name][i] # since i is the row number
+                if self.dataTypes[name] == "varchar":
+                    queryString += "'"
+
+                queryString += str(primary_key_vals[name][i]) # since i is the row number
+                if self.dataTypes[name] == "varchar":
+                    queryString += "'"
+
                 if l!=len(primary_cols_names)-1:
-                    queryString+=", "
+                    queryString+=" and "
                 else:
                     queryString+=";"
                 l+=1
@@ -156,12 +188,13 @@ class DisplayUpdateUI(uiabstract.ChildUI):
 
             # try to execute the query, if fails roll it back
             try:
-
+                print(queryString)
                 self.cursor.execute(queryString)
                 self.connection.commit()
 
-            except mysqlerrors.Error:
-                print("Update Failed")
+            except mysqlerrors.Error as e:
+
+                print(e)
 
 
 
