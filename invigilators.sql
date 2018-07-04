672157a899202e11c5c31c4e5f7fc9093091d37c
ashishthesatan <ashishthesatan@gmail.com>
2018-07-04 21:07:10 +0545

    updated the database sql according to changes that were made

--- invigilators.sql
+++ invigilators.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2018 at 07:16 PM
+-- Generation Time: Jul 04, 2018 at 05:20 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -34,6 +34,14 @@ CREATE TABLE `class` (
   `capacity` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`class_id`, `block_no`, `capacity`) VALUES
+(1, 23, 303),
+(2, 234, 303);
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +54,14 @@ CREATE TABLE `exam` (
   `programme` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `exam`
+--
+
+INSERT INTO `exam` (`subject_id`, `semester`, `programme`) VALUES
+(3, 42, 'bct'),
+(6, 5, 'bex');
+
 -- --------------------------------------------------------
 
 --
@@ -55,6 +71,14 @@ CREATE TABLE `exam` (
 CREATE TABLE `examhall` (
   `class_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `examhall`
+--
+
+INSERT INTO `examhall` (`class_id`) VALUES
+(1),
+(2);
 
 -- --------------------------------------------------------
 
@@ -68,6 +92,13 @@ CREATE TABLE `examin` (
   `subject_id` int(255) NOT NULL,
   `class_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `examin`
+--
+
+INSERT INTO `examin` (`semester`, `programme`, `subject_id`, `class_id`) VALUES
+(42, 'bct', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -84,6 +115,13 @@ CREATE TABLE `examinstance` (
   `no_attendees` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `examinstance`
+--
+
+INSERT INTO `examinstance` (`subject_id`, `semester`, `programme`, `shift`, `date`, `no_attendees`) VALUES
+(3, 42, 'bct', 1, '2018-06-13', 30);
+
 -- --------------------------------------------------------
 
 --
@@ -96,6 +134,14 @@ CREATE TABLE `invigilator` (
   `class_id` int(255) NOT NULL,
   `no_exams_invigilated` int(10) UNSIGNED ZEROFILL NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `invigilator`
+--
+
+INSERT INTO `invigilator` (`staff_id`, `date`, `class_id`, `no_exams_invigilated`) VALUES
+(1, '2018-06-05', 1, 0000000000),
+(2, '2018-06-11', 2, 0000000000);
 
 -- --------------------------------------------------------
 
@@ -110,6 +156,14 @@ CREATE TABLE `staff` (
   `lname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `phone` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`staff_id`, `faculty`, `fname`, `lname`, `phone`) VALUES
+(1, 'electronics and computer', 'aman', 'shakya', '9841414141'),
+(2, 'science and humanities', 'sujan', 'lamichane', '9841212121');
 
 -- --------------------------------------------------------
 
@@ -128,8 +182,10 @@ CREATE TABLE `subject` (
 --
 
 INSERT INTO `subject` (`subject_id`, `title`, `programme`) VALUES
-(1, 'database', 'bct'),
-(2, 'artificial intelligence', 'bct');
+(3, 'dbms1', 'bct'),
+(4, 'ai', 'bct'),
+(5, 'advanced electronics', 'bex'),
+(6, 'DSAP', 'bex');
 
 --
 -- Indexes for dumped tables
@@ -146,9 +202,9 @@ ALTER TABLE `class`
 --
 ALTER TABLE `exam`
   ADD PRIMARY KEY (`subject_id`,`semester`,`programme`),
-  ADD UNIQUE KEY `index_subject_id` (`subject_id`),
   ADD UNIQUE KEY `index_semester` (`semester`),
-  ADD UNIQUE KEY `index_programme` (`programme`);
+  ADD UNIQUE KEY `index_programme` (`programme`),
+  ADD UNIQUE KEY `subject_id` (`subject_id`,`semester`,`programme`);
 
 --
 -- Indexes for table `examhall`
@@ -161,16 +217,17 @@ ALTER TABLE `examhall`
 --
 ALTER TABLE `examin`
   ADD PRIMARY KEY (`semester`,`programme`,`subject_id`,`class_id`),
+  ADD UNIQUE KEY `semester` (`semester`,`programme`,`subject_id`,`class_id`),
   ADD KEY `index_class_id` (`class_id`) USING BTREE,
   ADD KEY `index_subject_id` (`subject_id`),
-  ADD KEY `index_programme` (`programme`),
-  ADD KEY `index_semester` (`semester`);
+  ADD KEY `index_programme` (`programme`);
 
 --
 -- Indexes for table `examinstance`
 --
 ALTER TABLE `examinstance`
   ADD PRIMARY KEY (`subject_id`,`semester`,`programme`,`shift`,`date`),
+  ADD UNIQUE KEY `subject_id` (`subject_id`,`semester`,`programme`,`shift`,`date`),
   ADD KEY `index_subject_id` (`subject_id`) USING BTREE,
   ADD KEY `index_date` (`date`) USING BTREE,
   ADD KEY `index_shift` (`shift`) USING BTREE,
@@ -182,7 +239,7 @@ ALTER TABLE `examinstance`
 --
 ALTER TABLE `invigilator`
   ADD PRIMARY KEY (`staff_id`),
-  ADD KEY `class_id` (`class_id`);
+  ADD KEY `invigilator_ibfk_2` (`class_id`);
 
 --
 -- Indexes for table `staff`
@@ -195,6 +252,7 @@ ALTER TABLE `staff`
 --
 ALTER TABLE `subject`
   ADD PRIMARY KEY (`subject_id`,`programme`),
+  ADD UNIQUE KEY `subject_id` (`subject_id`,`programme`),
   ADD KEY `index_programme` (`programme`);
 
 --
@@ -205,19 +263,19 @@ ALTER TABLE `subject`
 -- AUTO_INCREMENT for table `class`
 --
 ALTER TABLE `class`
-  MODIFY `class_id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `class_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `staff_id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `staff_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `subject_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `subject_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
@@ -227,37 +285,38 @@ ALTER TABLE `subject`
 -- Constraints for table `exam`
 --
 ALTER TABLE `exam`
-  ADD CONSTRAINT `exam_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`);
+  ADD CONSTRAINT `exam_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `exam_ibfk_2` FOREIGN KEY (`programme`) REFERENCES `subject` (`programme`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `examhall`
 --
 ALTER TABLE `examhall`
-  ADD CONSTRAINT `examhall_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`);
+  ADD CONSTRAINT `examhall_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `examin`
 --
 ALTER TABLE `examin`
-  ADD CONSTRAINT `examin_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `examhall` (`class_id`),
-  ADD CONSTRAINT `examin_ibfk_2` FOREIGN KEY (`programme`) REFERENCES `exam` (`programme`),
-  ADD CONSTRAINT `examin_ibfk_3` FOREIGN KEY (`semester`) REFERENCES `exam` (`semester`),
-  ADD CONSTRAINT `examin_ibfk_4` FOREIGN KEY (`subject_id`) REFERENCES `exam` (`subject_id`);
+  ADD CONSTRAINT `examin_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `examin_ibfk_2` FOREIGN KEY (`programme`) REFERENCES `subject` (`programme`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `examin_ibfk_3` FOREIGN KEY (`semester`) REFERENCES `exam` (`semester`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `examin_ibfk_4` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `examinstance`
 --
 ALTER TABLE `examinstance`
-  ADD CONSTRAINT `examinstance_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `exam` (`subject_id`),
-  ADD CONSTRAINT `examinstance_ibfk_2` FOREIGN KEY (`semester`) REFERENCES `exam` (`semester`),
-  ADD CONSTRAINT `examinstance_ibfk_3` FOREIGN KEY (`programme`) REFERENCES `exam` (`programme`);
+  ADD CONSTRAINT `examinstance_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `examinstance_ibfk_2` FOREIGN KEY (`semester`) REFERENCES `exam` (`semester`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `examinstance_ibfk_3` FOREIGN KEY (`programme`) REFERENCES `subject` (`programme`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `invigilator`
 --
 ALTER TABLE `invigilator`
-  ADD CONSTRAINT `invigilator_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`staff_id`),
-  ADD CONSTRAINT `invigilator_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `examhall` (`class_id`);
+  ADD CONSTRAINT `invigilator_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`staff_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `invigilator_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
